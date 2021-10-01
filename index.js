15e74bf2ecba3f9bdde8540fc10e64c3d90e119e
samualdi <samualdick89@gmail.com>
2021-10-01 14:49:30 +0100

    Updated index.js to give list of airport options based on the contents of the API

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 const inquirer = require("inquirer");
-const { planTripToAirport } = require('./utils/plan-trip-to-airport');
-const { findShortestFlights } = require('./utils/find-shortest-flights');
 const { createTripData } = require('./utils/create-trip-data');
+const { getAirportData} = require('./utils/get-airport-data');
 
 inquirer
-    .prompt([
-        {
-            name: "distanceToAirport",
-            message: "How far away is your origin airport?",
-            type: "number",
-        },
-        {
-            name: "passengers",
-            message: "How many passengers are travelling?",
-            type: "input",
-        },
-        {
-            name: "originAirport",
-            message: "What is your origin airport?",
-            type: "input",
-        },
-        {
-            name: "destinationAirport",
-            message: "What is your departure airport?",
-            type: "input",
-        },
-    ])
-    .then((answers) => {
-        return createTripData(answers.distanceToAirport, answers.passengers, answers.originAirport, answers.destinationAirport);
-    }).then((result) => {
-        console.info(`You should travel to the airport by -> ${result.tripToAirport.modeOfTransport}`);
-        console.info(`Your outbound flight route is -> ${result.flightData.outboundFlight.journey}`);
-        console.info(`Your inbound flight route is -> ${result.flightData.inboundFlight.journey}`);
-        console.info(`The total cost of your trip is -> £${result.totalCost}`);
-    });
+  .prompt([
+    {
+      name: "distanceToAirport",
+      message: `How far away is your origin airport?`,
+      type: "number",
+    },
+    {
+      name: "passengers",
+      message: "How many passengers are travelling?",
+      type: "number",
+    },
+    {
+      name: "originAirport",
+      message: "What is your origin airport?",
+      type: "list",
+      choices: async function () {
+        return await getAirportData();
+      },
+    },
+    {
+      name: "destinationAirport",
+      message: "What is your departure airport?",
+      type: "list",
+      choices: async function () {
+        return await getAirportData();
+      },
+    },
+  ])
+  .then((answers) => {
+    const originAirport = answers.originAirport.substring(0, 3);
+    const destinationAirport = answers.destinationAirport.substring(0, 3);
+    return createTripData(
+      answers.distanceToAirport,
+      answers.passengers,
+      originAirport,
+      destinationAirport
+    );
+  })
+  .then((result) => {
+    console.info(
+      `You should travel to the airport by -> ${result.tripToAirport.modeOfTransport}`
+    );
+    console.info(
+      `Your outbound flight route is -> ${result.flightData.outboundFlight.journey}`
+    );
+    console.info(
+      `Your inbound flight route is -> ${result.flightData.inboundFlight.journey}`
+    );
+    console.info(`The total cost of your trip is -> £${result.totalCost}`);
+  });
+  
 
        
